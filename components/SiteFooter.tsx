8dda0d87ff5a9e5bd421f5bc2696d8dcab269b65
hideaki-yawata <hideaki.yawata@gmail.com>
2026-08-04 15:19:40 +0200

    change text on footer

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -1,8 +1,14 @@
+"use client";
+
+const SITE_START_YEAR = 2020;
+
 export function SiteFooter() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="flex h-12 items-center justify-center bg-text px-4 text-background md:h-[60px]">
       <p className="text-xs leading-[1.5]">
-        © Hideaki Yawata 2026
+        © Hideaki Yawata {SITE_START_YEAR}-{currentYear}
       </p>
     </footer>
   );
