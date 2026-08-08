9eb013847d6c89d2a4ca25252ff13c09acdaed12
hideaki-yawata <hideaki.yawata@gmail.com>
2026-08-08 16:28:12 +0200

    add hover animation to home link

--- components/HomeLink.tsx
+++ components/HomeLink.tsx
@@ -7,14 +7,16 @@ export function HomeLink() {
   return (
     <Link
       href="/"
-      className={`${plusJakartaItalic.className} inline-flex items-end gap-0.5 border-b border-accent text-xs font-bold leading-[1.5] text-accent md:text-base`}
+      className={`${plusJakartaItalic.className} group inline-flex items-end gap-0.5 border-b border-accent text-xs font-bold leading-[1.5] text-accent hover:border-transparent md:text-base`}
     >
-      <IconImage
-        src={images.icons.homeLinkArrow}
-        width={16}
-        height={16}
-        className="-rotate-90 md:h-[22px] md:w-[22px]"
-      />
+      <span className="inline-flex transition-transform duration-300 ease-in-out group-hover:-translate-x-1 group-focus-visible:-translate-x-1">
+        <IconImage
+          src={images.icons.homeLinkArrow}
+          width={16}
+          height={16}
+          className="-rotate-90 md:h-[22px] md:w-[22px]"
+        />
+      </span>
       Home
     </Link>
   );
